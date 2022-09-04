9c496c3433b30216feaadc108016e58a4f1e8d7d
unknown <Alamin>
2022-09-04 19:37:55 +0600

    middleware added

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,16 @@
+const err = {};
+
+err.errorHandler = (err, _, res, __) => {
+  console.log({
+    name: err.name,
+    message: err.message,
+    stack: err.stack,
+  });
+  res.status(500).json({
+    success: false,
+    message: "Internal Server Error",
+    error: err.message,
+  });
+};
+
+module.exports = err;
