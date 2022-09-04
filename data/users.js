4ecc887e582273db50c0530cdf798c6c27596671
unknown <Alamin>
2022-09-04 19:23:33 +0600

    fake data added

---
+++ data/users.js
@@ -0,0 +1,74 @@
+[
+  {
+    _id: "u11",
+    name: "Jhankar Mahbub",
+    gender: "Male",
+    contact: "01784512101",
+    address: "America",
+    photoURL:
+      "https://www.tbsnews.net/sites/default/files/styles/infograph/public/images/2021/07/08/img_2842_0.jpg?itok=G1cRamCf&timestamp=1625721391",
+  },
+  {
+    _id: "u12",
+    name: "Sumit Saha",
+    gender: "Male",
+    contact: "013215142513",
+    address: "Dhaka",
+    photoURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGFxKzcjpyghupAxhLdUoFE-_frcKd-QolNA&usqp=CAU",
+  },
+  {
+    _id: "u13",
+    name: "Kamrul Islam",
+    gender: "Male",
+    contact: "01715412345",
+    address: "Dhaka",
+    photoURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7WjN3i7hzUpvx1ownjpKeK6HDGbvQps_p3g&usqp=CAU",
+  },
+  {
+    _id: "u14",
+    name: "Fazle Rabbi",
+    gender: "Male",
+    contact: "01621454785",
+    address: "Dhaka",
+    photoURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRihEiOoAnsNCctcO7vT-MKNuwt4trSiDBaxw&usqp=CAU",
+  },
+  {
+    _id: "u15",
+    name: "Ashraful Islam",
+    gender: "Male",
+    contact: "01854751424",
+    address: "Cumilla",
+    photoURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXvATMeCZkUa40hrRMTGFhnUOecAgtUnVx4A&usqp=CAU",
+  },
+  {
+    _id: "u16",
+    name: "Rajaul Karim Chowdhury",
+    gender: "Male",
+    contact: "01521457874",
+    address: "Dhaka",
+    photoURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWXLbaADRRfjkI4anuWQfABJkON-FH4ILjEg&usqp=CAU",
+  },
+  {
+    _id: "u17",
+    name: "Md Golam Kibria",
+    gender: "Male",
+    contact: "01713456701",
+    address: "Dhaka",
+    photoURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0qKmsR0Ou9OS1LfbABycxKRewrakHkc3tBg&usqp=CAU",
+  },
+  {
+    _id: "u18",
+    name: "MD Aktaruzzaman",
+    gender: "Male",
+    contact: "01521457841",
+    address: "Dhaka",
+    photoURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtgW8NPGKWemekL5t6D5PdOBvWjzjJpHvvNg&usqp=CAU",
+  },
+];
